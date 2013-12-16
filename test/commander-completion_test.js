5ce743c5836b52ef56037d1956212efa4ae40ea7
Todd Wolfson <todd@twolfson.com>
2013-12-16 02:54:56 -0800

    First test is passing

--- test/commander-completion_test.js
+++ test/commander-completion_test.js
@@ -1,29 +1,34 @@
+var assert = require('assert');
 var CommanderCompletion = require('../');
 
 describe('A commander with completable commands', function () {
   before(function () {
-    this.program = new CommanderCompletion()
-                     .command('hello')
-                     .completion(function (params, cb) {
-                       cb(null, ['world']);
-                     })
-                     .action(function () {});
+    this.program = new CommanderCompletion();
+    this.program
+      .command('hello')
+      .completion(function (params, cb) {
+        cb(null, ['world']);
+      })
+      .action(function () {});
   });
 
   describe('when completing an incomplete command', function () {
     before(function (done) {
+      // Complete our input and save results
+      var that = this;
       this.program.complete({
         // TODO: Use same lib as from completion
         // `node test hel|`
-        line: 'node test hel',
-        cursor: 'node test hel'.length
+        line: 'hel',
+        cursor: 'hel'.length
       }, function saveResult (err, results) {
-
+        that.results = results;
+        done(err);
       });
     });
 
     it('completes the command', function () {
-
+      assert.deepEqual(this.results, ['hello']);
     });
   });
 
