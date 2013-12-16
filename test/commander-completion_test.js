152dd7506f1f41f1bee47f5c25ad7289dcf887b9
Todd Wolfson <todd@twolfson.com>
2013-12-16 02:43:47 -0800

    Filling out tests

--- test/commander-completion_test.js
+++ test/commander-completion_test.js
@@ -1,11 +1,35 @@
-var commander_completion = require('../');
+var CommanderCompletion = require('../');
 
-describe('commander-completion', function () {
+describe('A commander with completable commands', function () {
   before(function () {
-
+    this.program = new CommanderCompletion()
+                     .command('hello')
+                     .completion(function (params, cb) {
+                       cb(null, ['world']);
+                     })
+                     .action(function () {});
   });
 
-  it('', function () {
+  describe('when completing an incomplete command', function () {
+    before(function (done) {
+      this.program.complete({
+        // TODO: Use same lib as from completion
+        // `node test hel|`
+        line: 'node test hel',
+        cursor: 'node test hel'.length
+      }, function saveResult (err, results) {
 
+      });
+    });
+
+    it('completes the command', function () {
+
+    });
+  });
+
+  describe('when completing a command\'s results', function () {
+    it('completes the command\'s results', function () {
+
+    });
   });
 });
