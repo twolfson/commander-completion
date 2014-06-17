30c89dec956e4e6e29096496b79d3a5cb707a6a5
Todd Wolfson <todd@twolfson.com>
2014-06-17 03:35:00 -0700

    Pulled over option work from 89dd2169fd023bc82898e3cb54fdb5ca52a5f1a9

--- test/commander-completion_test.js
+++ test/commander-completion_test.js
@@ -53,3 +53,50 @@ describe('A commander with completable commands', function () {
     });
   });
 });
+
+describe('A commander with options', function () {
+  before(function () {
+    this.program = new CommanderCompletion();
+    this.program.name = 'wat';
+    this.program.option('-n, --dry-run', 'Output commands but do nothing');
+    this.program
+      .command('hello')
+      .completion(function (params, cb) {
+        cb(null, ['world']);
+      })
+      .action(function () {});
+  });
+
+  describe.only('completing a command with options', function () {
+    before(function (done) {
+      // Complete our input and save results
+      var that = this;
+      this.program.complete({
+        // TODO: Use same lib as from completion
+        // `wat -n hel|`
+        line: 'wat -n hel',
+        cursor: 'wat -n hel'.length
+      }, function saveResult (err, results) {
+        that.results = results;
+        done(err);
+      });
+    });
+
+    it('completes the command', function () {
+      assert.deepEqual(this.results, ['hello']);
+    });
+  });
+
+  describe.skip('completing a command without options', function () {
+    it('completes the command', function () {
+    });
+  });
+});
+
+describe.skip('A commander with an option that has a required value', function () {
+  // hello --dir <cwd>
+});
+
+describe.skip('A commander with an option that has a optional value', function () {
+  // hello --dir [cwd]
+});
